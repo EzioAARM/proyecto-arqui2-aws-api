3674d4ff2972e579bf32b52014702f083f0e6d9e
Axel Rodriguez <alejandrom9712@gmail.com>
2019-10-22 14:29:23 -0600

    api para insertar arreglada

--- api-lambda/insert-dynamo/index.js
+++ api-lambda/insert-dynamo/index.js
@@ -1,70 +1,30 @@
-const AWS = require('aws-sdk');
-const moment = require('moment');
-const ddb = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10'
-});
-const dynamoTableName = process.env.TABLE_NAME;
-
-exports.handler = (event, context, callback) => {
-    let connectionData;
-    
-    try {
-        connectionData = await ddb.scan({ 
-            TableName: TABLE_NAME, 
-            ProjectionExpression: 'connectionId' })
-            .promise();
-    } catch (e) {
-        return { 
-            statusCode: 500, 
-            body: e.stack 
-        };
-    }
-
-    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-        apiVersion: '2018-11-29',
-        endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
-    });
-
-    const postData = JSON.parse(event.body).data;
-
-    const postCalls = connectionData.Items.map(async ({ connectionId }) => {
-        try {
-            await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
-            console.log(postData);
-        } catch (e) {
-            console.log("Se ha perdido la conexión");
-        }
-    });
-
-
-    try {
-        await Promise.all(postCalls);
-    } catch (e) {
-        return { 
-            statusCode: 500, 
-            body: e.stack 
-        };
-    }
-
-    return { 
-        statusCode: 200, 
-        body: 'Información enviada' 
-    };
-
-    
-    /*AWS.config.update({region: 'us-east-1'});
+var AWS = require('aws-sdk');
+var moment = require('moment');
+exports.handler = async (event) => {
+    AWS.config.update({region: 'us-east-1'});
+    var ddb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+    JSON.stringify(event, null, 2);
+    console.log(event);
     var params = {
         Item: {
-            id: moment().unix(),
-            dato: postData.dato
+        "modified-date": moment().unix().toString(),
+        "dato": event.queryStringParameters.dato
         },
-        TableName: 'arqui-lab'
+        TableName: process.env.TABLE_NAME
     };
+    console.log(params);
     let putItem = new Promise((res, rej) => {
         ddb.put(params, function(err, data) {
-            return result.status(200);
+            if (err) {
+                rej(err);
+            } else {
+                res({
+                    statusCode: 200
+                });
+            }
         }); 
     });
-    const result = await putItem;
-    return result.status(200);*/
+
+    const result = await putItem; 
+    return result;
 };
