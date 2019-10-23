f79faa40fc2cd523b11bec0b10ccc3be280c6716
Axel Alejandro Rodríguez Montenegro <alejandrom9712@gmail.com>
2019-10-23 02:30:22 -0600

    api para hacer petición https

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,101 +1,60 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h> 
-#include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 
-//Network parameters
+//parametros de red wifi
 const char* ssid = "Wifiaxel";
 const char* password = "123456789";
-const int httpsPort = 443;
-const char* host = "https://poywhb3qv6.execute-api.us-east-1.amazonaws.com/testing/";
-const char fingerPrint[] PROGMEM = "72 D4 00 92 77 37 50 C9 9B A1 38 FA 21 8A 9B FD BA CF CD 49";
-const int rounds = 0;
 
 //intervalos
 unsigned long prevMilis = 0;
-const long interval = 100;
+const long interval = 1000;
+
+int dato = 0;
+int cont = 0;
+int inser = 0;
+int hecho = 0;
 
 void setup () {
-  pinMode(5,INPUT);               //D1 as input
-  delay(1000);
-  Serial.begin(115200);           //Baudios
-  WiFi.mode(WIFI_OFF);            //Prevents reconnection issue (taking too long to connect)
-  delay(1000);
-  WiFi.mode(WIFI_STA);            //Only Station No AP, This line hides the viewing of ESP as wifi hotspot
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-  Serial.println("");
-
-  Serial.print("Connecting");
-  // Wait for connection
+  //PMOD CONFIG
+  pinMode(5,INPUT);//D1
+  //Baudios
+  Serial.begin(115200);
+  //conectar a WiFi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("Connecting...");
+    delay(1000);
+    Serial.print("Estableciendo una conexion..\n");
   }
- 
-  //If connection successful show IP address in serial monitor
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+  Serial.print("Conexion establecida!\n");
+
+  HTTPClient http2;  
+    http2.begin("http://ec2-18-188-175-99.us-east-2.compute.amazonaws.com:3000/");
+    int httpCode2 = http2.GET();                                                                  
+    if (httpCode2 > 0) { //Check the returning code
+      String payload2 = http2.getString();   //Get the request response payload
+      Serial.println(payload2);             //Print the response payload
+    }
+    http2.end();   //Close connection
+  
 }
 
 void loop() {
-  WiFiClientSecure httpsClient;    //Declare object of class WiFiClient
-
-  Serial.println(host);
-
-  Serial.printf("Using fingerPrint '%s'\n", fingerPrint);
-  httpsClient.setFingerprint(fingerPrint);
-  httpsClient.setTimeout(15000); // 15 Seconds
-  delay(interval);
-  
-  Serial.print("HTTPS Connecting");
-  int r=0; //retry counter
-  while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
-      delay(interval);
-      Serial.print(".");
-      r++;
-  }
-  if(r==30) {
-    Serial.println("Connection failed");
-  }
-  else {
-    Serial.println("Connected to web");
-  }
-  
-  String getData, Link;
-  int adcvalue=analogRead(A0);  //Read Analog value of LDR
-
-  //GET Data
-  Link = "modulo-esp?dato=" + rounds;
-
-  Serial.print("requesting URL: ");
-  Serial.println(host+Link);
-
-  httpsClient.print(String("GET ") + Link + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +               
-               "Connection: close\r\n\r\n");
-
-  Serial.println("request sent");
-                  
-  while (httpsClient.connected()) {
-    String line = httpsClient.readStringUntil('\n');
-    if (line == "\r") {
-      Serial.println("headers received");
-      break;
+  unsigned long cMilis = millis();
+  if(cMilis - prevMilis >= interval){
+    prevMilis = cMilis;
+    cont = digitalRead(5);
+    if (cont == HIGH) {
+      HTTPClient http;  //Declare an object of class HTTPClient
+      http.begin("http://ec2-18-188-175-99.us-east-2.compute.amazonaws.com:3000/" + datoNuevo);  //Direccion url de la instancia de amazon
+      int httpCode = http.GET();                                                                  //se hace la peticion GET
+      if (httpCode > 0) { //Check the returning code
+        Serial.println(httpCode);
+        String payload = http.getString();   
+        Serial.println(payload);             
+      }
+      http.end();
+    } else {
+      Serial.println("no se guardo nada");  
     }
   }
-
-  Serial.println("reply was:");
-  Serial.println("==========");
-  String line;
-  while(httpsClient.available()){        
-    line = httpsClient.readStringUntil('\n');  //Read Line by Line
-    Serial.println(line); //Print response
-  }
-  Serial.println("==========");
-  Serial.println("closing connection");
-    
-  delay(2000);  //GET Data at every 2 seconds
 }
